2cd75c6a182a65ac393e4fdfabb06c62aa9475e8
Jose Gazzano <jose.gazzano@fing.edu.uy>
2016-06-25 20:21:43 -0300

    deleting old files

--- js/angular-underscore.js
+++
@@ -1,4 +0,0 @@
-angular.module('underscore', [])
-.factory('_', ['$window', function($window) {
-  return $window._; // assumes underscore has already been loaded on the page
-}]);
--- js/controllers/2-controller.js
+++
@@ -1,4 +0,0 @@
-angular.module('saan.controllers')
-.controller('2Ctrl', function($scope) { 
-	$scope.activityId = '2';
-});
