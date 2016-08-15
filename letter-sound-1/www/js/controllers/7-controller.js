862257e6cee774d28bbb96ec7e53461957137429
Jose Gazzano <jose.gazzano@toptierlabs.com>
2016-08-15 20:44:59 -0300

    adding service and controller for the activity

--- letter-sound-1/www/js/controllers/7-controller.js
+++ letter-sound-1/www/js/controllers/7-controller.js
@@ -1,5 +1,36 @@
 angular.module('saan.controllers')
-.controller('7Ctrl', function($scope) {
+.controller('7Ctrl', ['$scope', 'DeckBuilder', 'Util', function($scope, DeckBuilder, Util) {
 	$scope.activityId = '7';
-		
-});
+	$scope.config = '';
+	$scope.deck = [];
+
+	function getConfiguration(level){
+		DeckBuilder.getConfig(level).then(function(data){
+			$scope.config = data;
+			buildDeck(data.level.numberOfCards);
+		});
+	}
+
+	function buildDeck(numberOfCards){
+		var cards = [];
+		for (var i = 0; i < numberOfCards; i++){
+			var number = Util.getRandomNumber(20);
+			while (_.indexOf(cards, number) != -1)
+				number = Math.floor(Math.random() * 10);
+			cards.push(number);
+		}
+		var deck = [];
+		deck.push(cards);
+
+		for (i = 1; i < numberOfCards; i++){
+			deck.push(_.shuffle(cards));
+		}
+
+		$scope.deck = deck;
+		console.log(deck);
+
+	}
+
+	getConfiguration(1);
+
+}]);
---
+++ letter-sound-1/www/js/services/7-service.js
@@ -0,0 +1,24 @@
+angular.module('saan.services')
+.factory('DeckBuilder', function($http, Util) {
+  return {
+    getConfig: function(level) {
+      var src = 'data/7-config.json';
+      return $http.get(src).then(
+        function success(response) {
+          var data = response.data;
+          console.log(data);
+          return {
+            instructions : data.instructions,
+            errorMessages : data.errorMessages,
+            successMessages: data.successMessages,
+            level: data.levels[level-1]
+          };
+        },
+        function error() {
+          //TODO: handle errors for real
+          console.log("error");
+        }
+      );
+    }
+  };
+});
