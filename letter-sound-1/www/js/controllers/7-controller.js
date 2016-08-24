e6e0667c5735bc914abfa502cd18db2beeccb21d
Jose Gazzano <jose.gazzano@toptierlabs.com>
2016-08-24 18:49:18 -0300

    increasing font size and reloading when completed

--- letter-sound-1/www/js/controllers/7-controller.js
+++ letter-sound-1/www/js/controllers/7-controller.js
@@ -1,45 +1,54 @@
-angular.module('saan.controllers')
-.controller('7Ctrl', ['$scope', 'DeckBuilder', 'Util', function($scope, DeckBuilder, Util) {
-	$scope.activityId = '7';
-	$scope.config = '';
-	$scope.deck = [];
-	$scope.map = [];
+(function() {
+  'use strict';
+	angular.module('saan.controllers')
+	.controller('7Ctrl', ['$scope', 'DeckBuilder', 'Util', function($scope, DeckBuilder, Util) {
+		$scope.activityId = '7';
+		$scope.deck = [];
+		$scope.map = [];
+		$scope.size = 0;
 
-	function getConfiguration(level){
-		DeckBuilder.getConfig(level).then(function(data){
-			$scope.config = data;
-			buildDeck(parseInt(data.level.numberOfCards));
-		});
-	}
+		var config = '';
 
-	function buildDeck(size){
-		var cards = [];
-		var auxCards = [];
-
-		for (var i = 0; i < size * 2; i++){
-			var number = Util.getRandomNumber(20);
-			while (_.indexOf(auxCards, number) != -1)
-				number = Math.floor(Math.random() * 10);
-			auxCards.push(number);
-			cards.push({key: number, value: number});
-			cards.push({key: number, value: Util.numberToWords(number)});
+		function getConfiguration(level){
+			DeckBuilder.getConfig(level).then(function(data){
+				config = data;
+				$scope.size = parseInt(data.level.numberOfCards);
+				$scope.buildDeck();
+			});
 		}
 
-		cards = _.shuffle(cards);
-		console.log(cards);
-		var deck = [];
-		var deckMap = [];
-		var start = 0;
-		var top = size;
-		for (var j = 0; j < size; j++){
-			deck.push(cards.splice(start, size));
-			deckMap.push(_.map(deck[j], function(num){ return 0; }));
-		}
-		$scope.map = deckMap;
-		$scope.deck = deck;
-		console.log($scope.deck);
-	}
+		$scope.buildDeck = function(){
+			var cards = [];
+			var auxCards = [];
 
-	getConfiguration(1);
+			for (var i = 0; i < $scope.size / 2; i++){
+				var number = Util.getRandomNumber(parseInt(config.level.numberRange));
 
-}]);
+				while (_.indexOf(auxCards, number) != -1)
+					number = Math.floor(Math.random() * 10);
+
+				auxCards.push(number);
+				cards.push({key: number, value: number});
+				cards.push({key: number, value: Util.numberToWords(number)});
+			}
+
+			cards = _.shuffle(cards);
+
+			var deck = [];
+			var deckMap = [];
+			var start = 0;
+			var deckDimension = Math.sqrt($scope.size);
+
+			for (var j = 0; j < deckDimension; j++){
+				deck.push(cards.splice(start, deckDimension));
+				deckMap.push(_.map(deck[j], function(num){ return 0; }));
+			}
+
+			$scope.map = deckMap;
+			$scope.deck = deck;
+		};
+
+		//TODO: Get current level in order to get the proper configuration
+		getConfiguration(1);
+	}]);
+})();
