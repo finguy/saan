f819946622176f9ffce1986953696874e1fa34aa
Jose Gazzano <jose.gazzano@toptierlabs.com>
2016-08-24 18:49:18 -0300

    increasing font size and reloading when completed

--- letter-sound-1/www/js/directives/flipableCard.js
+++ letter-sound-1/www/js/directives/flipableCard.js
@@ -1,26 +1,29 @@
-angular.module('saan.directives')
-.directive('flipableCard', function(){
-  return {
-    require: '^^memoryDeck',
-    restrict: "E",
-    templateUrl: "templates/directives/flipableCard.html",
-    scope: {
-      card: '=',
-      row: '=',
-      col: '='
-    },
-    link: function(scope, element, attrs, deckCtrl){
-      scope.flipCard = function(){
-        deckCtrl.flipCard(scope.row, scope.col);
-      };
+(function() {
+  'use strict';
+  angular.module('saan.directives')
+  .directive('flipableCard', function(){
+    return {
+      require: '^^memoryDeck',
+      restrict: "E",
+      templateUrl: "templates/directives/flipableCard.html",
+      scope: {
+        card: '=',
+        row: '=',
+        col: '='
+      },
+      link: function(scope, element, attrs, deckCtrl){
+        scope.flipCard = function(){
+          deckCtrl.flipCard(scope.row, scope.col);
+        };
 
-      scope.isCardFlipped = function(){
-        return deckCtrl.isCardFlipped(scope.row, scope.col);
-      };
+        scope.isCardFlipped = function(){
+          return deckCtrl.isCardFlipped(scope.row, scope.col);
+        };
 
-      scope.isCardMatched = function(){
-        return deckCtrl.isCardMatched(scope.row, scope.col);
-      };
-    }
-  };
-});
+        scope.isCardMatched = function(){
+          return deckCtrl.isCardMatched(scope.row, scope.col);
+        };
+      }
+    };
+  });
+})();
--- letter-sound-1/www/js/directives/memoryDeck.js
+++ letter-sound-1/www/js/directives/memoryDeck.js
@@ -1,54 +1,70 @@
-angular.module('saan.directives')
-.directive('memoryDeck', function(){
-  return {
-    restrict: "E",
-    templateUrl: "templates/directives/memoryDeck.html",
-    scope: {
-      deck: '=',
-      map: '='
-    },
-    controller: ['$scope', function deckController($scope){
-      var rows = $scope.deck.length;
-      var selectedCard = {row: "", col: ""};
-      var flipEnabled = true;
+(function() {
+  'use strict';
+  angular.module('saan.directives')
+  .directive('memoryDeck', function(){
+    return {
+      restrict: "E",
+      templateUrl: "templates/directives/memoryDeck.html",
+      scope: {
+        deck: '=',
+        map: '=',
+        build: '&',
+        size: '='
+      },
+      controller: ['$scope', function deckController($scope){
+        var CARD_BACK = 0;
+        var CARD_FRONT = 1;
+        var CARD_MATCHED = 2;
+        var CARD_CHECK_DELAY = 1000;
 
-      this.CARD_BACK = 0;
-      this.CARD_FRONT = 1;
-      this.CARD_SOLVED = 2;
+        var rows = $scope.deck.length;
+        var selectedCard = {row: "", col: ""};
+        var flipEnabled = true;
+        var matchedCards = 0;
 
-      this.isCardFlipped = function(row, col){
-        return $scope.map[row][col] == 1;
-      };
+        this.isCardFlipped = function(row, col){
+          return $scope.map[row][col] == 1;
+        };
 
-      this.flipCard = function(row, col){
-        if (flipEnabled && $scope.map[row][col] === 0){
-          $scope.map[row][col] = 1;
-          if (selectedCard.row === ""){
-            selectedCard.row = row;
-            selectedCard.col = col;
-          }else{
-            flipEnabled = false;
-            setTimeout(function(){
-              $scope.$apply(function(){
-                var value = $scope.deck[row][col].key != $scope.deck[selectedCard.row][selectedCard.col].key ? 0 : 2;
-                $scope.map[row][col] = value;
-                $scope.map[selectedCard.row][selectedCard.col] = value;
-                selectedCard.row = "";
-                selectedCard.col = "";
-                flipEnabled = true;
-              });
-            }, 1000);
+        this.flipCard = function(row, col){
+          if (flipEnabled && $scope.map[row][col] === CARD_BACK){
+            $scope.map[row][col] = CARD_FRONT;
+            if (selectedCard.row === ""){
+              selectedCard.row = row;
+              selectedCard.col = col;
+            }else{
+              flipEnabled = false;
+              setTimeout(function(){
+                $scope.$apply(function(){
+                  if ($scope.deck[row][col].key == $scope.deck[selectedCard.row][selectedCard.col].key){
+                    matchCard(row, col);
+                    matchCard(selectedCard.row, selectedCard.col);
+                  }
+                  else{
+                    $scope.map[row][col] = CARD_BACK;
+                    $scope.map[selectedCard.row][selectedCard.col] = CARD_BACK;
+                  }
+                  selectedCard.row = "";
+                  selectedCard.col = "";
+                  flipEnabled = true;
+                });
+              }, CARD_CHECK_DELAY);
+            }
           }
-        }
-      };
+        };
 
-      this.matchCard = function(row, col){
-        $scope.map[row][col] = 2;
-      };
+        this.isCardMatched = function(row, col){
+          return $scope.map[row][col] == CARD_MATCHED;
+        };
 
-      this.isCardMatched = function(row, col){
-        return $scope.map[row][col] == 2;
-      };
-    }]
-  };
-});
+        var matchCard = function(row, col){
+          $scope.map[row][col] = CARD_MATCHED;
+          matchedCards++;
+          if (matchedCards == $scope.size){
+            $scope.build();
+          }
+        };
+      }]
+    };
+  });
+})();
