8f47888300a63c747de44075988b784f00b04a53
Jose Gazzano <jose.gazzano@fing.edu.uy>
2016-09-04 21:23:13 -0300

    adding instructions and transitions

--- letter-sound-1/www/js/controllers/9-controller.js
+++ letter-sound-1/www/js/controllers/9-controller.js
@@ -2,9 +2,16 @@
   'use strict';
 
 	angular.module('saan.controllers')
-	.controller('9Ctrl',['$scope','Util', function($scope, Util) {
+	.controller('9Ctrl',['$scope','Util', 'TTSService', function($scope, Util, TTSService) {
 		$scope.activityId = '9';
     $scope.dropzone = [];
+    $scope.items = ['coso'];
+    $scope.step = 1;
+
+    var instructions = ["this is the number two", "And this is how its written"];
+    var totalSteps = 3;
+    var itemCount = 0;
+
 
     var config = '';
     var Ctrl9 = Ctrl9 || {} ;
@@ -14,8 +21,25 @@
     });
 
     Ctrl9.getConfiguration = function (level){
-
+      $scope.number = 2;
+      Ctrl9.startTutorial();
 		};
+
+    Ctrl9.startTutorial = function(){
+      $scope.dropzone = [];
+      $scope.step = 0;
+      itemCount = 0;
+      for (var i = 1; i <= totalSteps; i++){
+        setTimeout(function(){Ctrl9.readInstructions(i)}, i*2000);
+      }
+    };
+
+    Ctrl9.readInstructions = function(step){
+      $scope.$apply(function(){
+        console.log(instructions[step]);
+        $scope.step++;
+      });
+    };
 
     $scope.sortableOptions = {
       containment: '.activity-9-content',
@@ -24,7 +48,15 @@
 
     $scope.sortableCloneOptions = {
       containment: '.activity-9-content',
-      clone: true
+      clone: true,
+      itemMoved: function (eventObj) {
+        itemCount++;
+        console.log(itemCount);
+
+        if (itemCount == $scope.number){
+          Ctrl9.startTutorial();          
+        }
+      }
     };
 
 	}]);
