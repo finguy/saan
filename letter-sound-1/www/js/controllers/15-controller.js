e6729674d455ff1fac635030343ee1989283c708
Jose Gazzano <jose.gazzano@fing.edu.uy>
2016-10-16 16:36:26 -0300

    initial commit activity 15

---
+++ letter-sound-1/www/js/controllers/15-controller.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+	angular.module('saan.controllers')
+	.controller('14Ctrl',['$scope','Util', 'TTSService', 'NumberOperations',
+  function($scope, Util, TTSService, NumberOperations) {
+    $scope.activityId = '15';
+	}
+	]);
+})();
