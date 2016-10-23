9178a248d92bd786af78f91efcb20dda0d31e584
Jose Gazzano <jose.gazzano@fing.edu.uy>
2016-10-23 02:45:25 -0300

    initial commit for activity 15 json configuration file

--- letter-sound-1/www/js/controllers/15-controller.js
+++ letter-sound-1/www/js/controllers/15-controller.js
@@ -2,7 +2,7 @@
   'use strict';
 
 	angular.module('saan.controllers')
-	.controller('14Ctrl',['$scope','Util', 'TTSService', 'NumberOperations',
+	.controller('15Ctrl',['$scope','Util', 'TTSService', 'NumberOperations',
   function($scope, Util, TTSService, NumberOperations) {
     $scope.activityId = '15';
 	}
