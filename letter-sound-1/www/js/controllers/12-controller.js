e25eca3f229852abd4bc490c6c472d964d1ce7f2
Leandro Zeballos <leandro.zeballos@bairesdev.com>
2016-10-23 11:20:37 -0300

    Added activity for reading comprehension

---
+++ letter-sound-1/www/js/controllers/12-controller.js
@@ -0,0 +1,162 @@
+(function() {
+  'use strict';
+  angular.module('saan.controllers')
+  .controller('12Ctrl', function($scope ,RandomText, TTSService,
+    Util, Animations, Score,ActividadesFinalizadasService) {
+    $scope.activityId = '12'; // Activity Id
+
+    $scope.img = "";
+    $scope.assets = [];
+    $scope.playedTexts = [];
+    $scope.text = "";
+    $scope.instructions = ""; // Instructions to read
+    $scope.successMessages = [];
+    $scope.errorMessages  = [];
+
+    $scope.dashboard = []; // Dashboard letters
+
+    $scope.level = $scope.level || 1; // Indicates activity level
+    $scope.totalLevels = 3;
+    $scope.activityProgress = 0;
+    $scope.score = 0;
+    $scope.checkingAnswer = false;
+
+    //Reproduces sound using TTSService
+    $scope.speak = TTSService.speak;
+
+    var Ctrl12 = Ctrl12 || {};
+
+    Ctrl12.setUpLevel = function() {
+      var level = Util.getLevel($scope.activityId);
+      if (level) {
+        $scope.level = level;
+        $scope.activityProgress = 100 * (level-1)/$scope.totalLevels; // -1 porque empieza en cero.
+      }
+    };
+
+    Ctrl12.setUpScore = function(){
+      var score = Util.getScore($scope.activityId);
+      if (score) {
+        $scope.score = score;
+      }
+    };
+
+    Ctrl12.setUpStatus = function(){
+      var finished = Util.getStatus($scope.activityId);
+      if (finished === false || finished === true) {
+        $scope.finished = finished;
+      }
+    };
+
+    //Shows Activity Dashboard
+    Ctrl12.showDashboard = function(readInstructions) {
+
+      Ctrl12.setUpLevel();
+      Ctrl12.setUpScore();
+      Ctrl12.setUpStatus();
+
+      RandomText.text($scope.level, $scope.playedTexts).then(
+        function success(data) {
+          console.log(data);
+          Ctrl12.setUpContextVariables(data);
+          var readWordTimeout = (readInstructions) ? 2000 : 1000;
+
+          //wait for UI to load
+          setTimeout(function() {
+            if (readInstructions){
+              $scope.speak($scope.instructions);
+                setTimeout(function() {
+                  $scope.speak($scope.wordStr);
+                }, 3000);
+            } else {
+              $scope.speak($scope.wordStr);
+            }
+          }, readWordTimeout);
+
+        },
+        function error(error) {
+          console.log(error);
+        }
+      );
+    };
+
+    Ctrl12.setUpContextVariables = function(data) {
+      var textJson = data.textJson;
+      var position = Util.getRandomNumber(textJson.questions.length);
+      $scope.playedTexts.push(textJson.id);
+      $scope.text = textJson.text;
+
+      $scope.question = textJson.questions[position].question;
+      $scope.answer = textJson.questions[position].answer;
+      $scope.options = _.shuffle(textJson.questions[position].options);
+
+      $scope.instructions = data.instructions;
+      $scope.successMessages = data.successMessages;
+      $scope.errorMessages = data.errorMessages;
+
+      $scope.assets = data.assets;
+      $scope.addScore = data.scoreSetUp.add;
+      $scope.substractScore = data.scoreSetUp.substract;
+      $scope.minScore = data.scoreSetUp.minScore;
+      $scope.totalLevels = data.totalLevels;
+      $scope.checkingAnswer = false;
+
+    };
+
+    $scope.handleProgress = function(answer) {
+      var ER = new RegExp($scope.answer,"i");
+      var isAnswerOk = ER.test(answer);
+      if (isAnswerOk) {
+        $scope.checkingAnswer = true;
+        if (!$scope.finished) {
+          $scope.score = Score.update($scope.addScore, $scope.score);
+          Util.saveScore($scope.activityId, $scope.score);
+        }
+
+        var position = Util.getRandomNumber($scope.successMessages.length);
+        var successMessage = $scope.successMessages[position];
+        $scope.speak(successMessage);
+        setTimeout(function() {
+          Ctrl12.levelUp(); //Advance level
+          Util.saveLevel($scope.activityId, $scope.level);
+          if (!$scope.finished) { // Solo sumo o resto si no esta finalizada
+            $scope.finished = $scope.score >= $scope.minScore;
+            Util.saveStatus($scope.activityId, $scope.finished);
+            ActividadesFinalizadasService.add($scope.activityId);
+          }
+          Ctrl12.showDashboard(false); //Reload dashboard
+        }, 1000);
+      } else {
+        $scope.score = Score.update(-$scope.substractScore, $scope.score);
+        Util.saveScore($scope.activityId, $scope.score);
+        $scope.speak(name);
+        //wait for speak
+        setTimeout(function() {
+          var position = Util.getRandomNumber($scope.errorMessages.length);
+          var errorMessage = $scope.errorMessages[position];
+          $scope.speak(errorMessage);
+        }, 1000);
+      }
+    };
+
+    //Advance one level
+    Ctrl12.levelUp = function() {
+      $scope.level++;
+      $scope.letters = [];
+      $scope.dashboard = [];
+      $scope.selectedLetters = [];
+    };
+
+    // Goes back one level
+    Ctrl12.levelDown = function() {
+     $scope.level = (level > 1) ? (level - 1) : 1;
+      $scope.numbers = [];
+      $scope.dashboard = [];
+      $scope.selectedNumbers = [];
+    };
+
+    //*************** ACTIONS **************************/
+    //Show Dashboard
+    Ctrl12.showDashboard(true);
+  });
+})();
---
+++ letter-sound-1/www/js/services/12-service.js
@@ -0,0 +1,57 @@
+angular.module('saan.services')
+.factory('RandomText', function($http, LevelsTwelve, Util) {
+  return {
+    text: function(level, playedTexts) {
+      var src = LevelsTwelve.getSrcData(level);
+      return $http.get(src).then(
+        function success(response) {          
+          var data = response.data;
+          var json = data.readings;
+          var textsNotPlayed = [];
+          if (playedTexts.length === 0 ){
+            textsNotPlayed = json;
+          } else {
+            var playedTextsStr = playedTexts.toString();
+            for (var i in json) { //FIXME: try to use underscore
+              if (json[i].id) {
+                var ER = new RegExp(json[i].id, "i");
+                if (!ER.test(playedTextsStr) && json[i].id) {
+                  textsNotPlayed.push(json[i]);
+                }
+              }
+            }
+          }
+          var index = Util.getRandomNumber(textsNotPlayed.length);
+
+          return {
+            textJson: textsNotPlayed[index],
+            instructions : data.instructions,
+            errorMessages : data.errorMessages,
+            successMessages: data.successMessages,
+            scoreSetUp: data.scoreSetUp,
+            totalLevels : data.readings.length,
+          };
+        },
+        function error() {
+          //TODO: handle errors for real
+          console.log("error");
+        }
+      );
+    }
+  };
+})
+.factory('LevelsTwelve', function() {
+    return {
+      getSrcData: function(level) {
+        var src = '';
+        switch (level) {
+          case "1":
+            src = 'data/12-reading.json';
+            break;
+          default:
+            src = 'data/12-reading.json';
+        }
+        return src;
+      },
+    };
+});
