0118ca42994a84d074632bcebf947a00d3d5cc77
Jose Gazzano <jose.gazzano@fing.edu.uy>
2016-10-29 02:28:52 -0300

    activity15 flow

--- letter-sound-1/www/js/controllers/15-controller.js
+++ letter-sound-1/www/js/controllers/15-controller.js
@@ -1,9 +1,9 @@
 (function() {
   'use strict';
 
-	angular.module('saan.controllers')
-	.controller('15Ctrl',['$scope', '$log', 'MathOralProblems', 'AssetsPath',
-  function($scope, $log, MathOralProblems, AssetsPath) {
+  angular.module('saan.controllers')
+  .controller('15Ctrl', ['$scope', '$log', '$state', 'MathOralProblems', 'AssetsPath', 'ActividadesFinalizadasService',
+  function ($scope, $log, $state, MathOralProblems, AssetsPath, ActividadesFinalizadasService) {
     $scope.activityId = '15';
 
     var Ctrl15 = Ctrl15 || {};
@@ -26,12 +26,14 @@
     };
 
     Ctrl15.setActivity = function(stageData){
-			var instructionsPlayer = new Media(AssetsPath.sounds(stageData.soundPath),
-				function(){ $scope.$apply(Ctrl15.showOptions(stageData)); },
-				function(err){ console.log(err); }
+      $scope.options = [];
+
+      var instructionsPlayer = new Media(AssetsPath.sounds(stageData.soundPath),
+        function(){ $scope.$apply(Ctrl15.showOptions(stageData)); },
+        function(err){ console.log(err); }
       );
 
-			instructionsPlayer.play();
+      instructionsPlayer.play();
     };
 
     Ctrl15.getStage = function(stage){
@@ -39,32 +41,37 @@
     };
 
     Ctrl15.showOptions = function(stageData){
-      $scope.options = [];
-      $scope.options.push(stageData.answer);
+      var options = [];
+      options.push(stageData.answer);
 
       var number;
-
       for (var i = 1; i < config.options; i++){
         var valid = false;
-				while (!valid){
-					number = Math.floor(Math.random() * 10);
-          var index = _.findIndex($scope.options, function(option){return option === number;}, number);
+        while (!valid){
+          number = Math.floor(Math.random() * 10);
+          var index = _.findIndex(options, function(option){return option === number;}, number);
           valid = (number !== 0 && index === -1);
         }
 
-        $scope.options.push(number);
-			}
+        options.push(number);
+      }
 
-      $scope.options = _.shuffle($scope.options);
+      $scope.options = _.shuffle(options);
     };
 
     $scope.checkAnswer = function(value){
       if (value === Ctrl15.getStage(stage).answer){
         stage++;
-        Ctrl15.setActivity(Ctrl15.getStage(stage));
+        if (stage <= config.problems.length){
+          setTimeout(function(){
+            $scope.$apply(function(){ Ctrl15.setActivity(Ctrl15.getStage(stage));});
+          }, 1000);
+        }
+        else{
+          ActividadesFinalizadasService.add($scope.activityId);
+          $state.go('lobby');
+        }
       }
-    }
-	}
-
-	]);
+    };
+   }]);
 })();
--- letter-sound-1/www/js/services/15-service.js
+++ letter-sound-1/www/js/services/15-service.js
@@ -1,7 +1,16 @@
-angular.module('saan.services')
-.factory('MathOralProblems', function($http, $log) {
-  return {
-    getConfig: function(level) {
+(function() {
+  'use strict';
+  angular.module('saan.services')
+  .factory('MathOralProblems', MathOralProblems);
+
+  MathOralProblems.$inject = ['$http', '$log'];
+
+  function MathOralProblems($http, $log) {
+    return {
+      getConfig: getConfig
+    };
+
+    function getConfig(level) {
       var src = 'data/15-config.json';
       if (level >= 1){
         return $http.get(src).then(
@@ -18,5 +27,5 @@ angular.module('saan.services')
         $log.error("Invalid level value");
       }
     }
-  };
-});
+  }
+})();
