6b45250bbd683c8f4e94cad718df7a7a26c3977d
Jose Gazzano <jose.gazzano@toptierlabs.com>
2016-11-22 22:31:12 -0300

    activity 17 audio feedback

--- letter-sound-1/www/js/controllers/17-controller.js
+++ letter-sound-1/www/js/controllers/17-controller.js
@@ -18,10 +18,15 @@
     var level;
     var config;
     var instructionsPlayer;
+    var readInstructions;
+    var successPlayer;
+    var failurePlayer;
+    var checking = false;
 
     $scope.$on('$ionicView.beforeEnter', function() {
       stageNumber = 1;
       level = Util.getLevel($scope.activityId) || 1;
+      readInstructions = true;
       Ctrl17.getConfiguration(level);
     });
 
@@ -40,22 +45,27 @@
       NumberPattern.getConfig(level).then(function(data){
         config = data;
         Ctrl17.setActivity();
-        // play instructions of activity
-        instructionsPlayer = new Media(AssetsPath.getGeneralAudio() + config.instructionsPath,
-          function(){
-            instructionsPlayer.release();
-          },
-          function(err){ $log.error(err); }
-        );
+        if (readInstructions){
+          // play instructions of activity
+          instructionsPlayer = new Media(AssetsPath.getGeneralAudio() + config.instructionsPath,
+            function(){
+              instructionsPlayer.release();
+            },
+            function(err){ $log.error(err); }
+          );
 
-        instructionsPlayer.play();
+          instructionsPlayer.play();
+          readInstructions = false;
+        }
       });
     };
 
     Ctrl17.setActivity = function(){
+      checking = false;
       $scope.hideDropzone = false;
       $scope.patternOptions = [];
       $scope.patternLeft = [];
+      $scope.patternRight = [];
       $scope.mode = config.level.mode;
 
       if (config.level.mode == MODE_SEQUENCE){
@@ -111,7 +121,9 @@
       clone: $scope.mode == MODE_FILLIN,
       dragEnd: function(eventObj) {
         //check that item was correctly moved
-        return Ctrl17.checkDragEnd(eventObj.source.itemScope.modelValue);
+        if (Ctrl17.checkDragEnd(eventObj.source.itemScope.modelValue)){
+          Ctrl17.failure();
+        }
       },
       itemMoved: function (eventObj) {
         if ($scope.mode == MODE_FILLIN ||
@@ -125,35 +137,45 @@
     };
 
     Ctrl17.success =  function(){
-      $scope.hideDropzone = true;
+      if (!checking){
+        var successFeedback = NumberPattern.getSuccessAudio();
+        $scope.hideDropzone = true;
 
-      if (Ctrl17.stageFinished()){
-        stageNumber++;
-        $timeout(function(){
-          Ctrl17.setActivity();
-        }, 1000);
-      }
-      else {
-        if (level == NumberPattern.getMinLevel() &&
-          !ActividadesFinalizadasService.finalizada($scope.activityId)){
-          // if player reached minimum for setting activity as finished
-          ActividadesFinalizadasService.add($scope.activityId);
-          level++;
-          $state.go('lobby');
-        }
-        else {
-          if (level >= NumberPattern.getMaxLevel()){
-            level = 1;
-            $state.go('lobby');
-          }
-          else {
-            $timeout(function(){
-              stageNumber = 1;
-              Util.saveLevel($scope.activityId, ++level);
-              Ctrl17.getConfiguration(level);
-            }, 1000);
-          }
-        }
+        successPlayer = new Media(AssetsPath.getSuccessAudio($scope.activityId) + successFeedback.path,
+          function(){
+            if (Ctrl17.stageFinished()){
+              stageNumber++;
+              $timeout(function(){
+                Ctrl17.setActivity();
+              }, 1000);
+            }
+            else {
+              if (level == NumberPattern.getMinLevel() &&
+                !ActividadesFinalizadasService.finalizada($scope.activityId)){
+                // if player reached minimum for setting activity as finished
+                ActividadesFinalizadasService.add($scope.activityId);
+                level++;
+                $state.go('lobby');
+              }
+              else {
+                if (level >= NumberPattern.getMaxLevel()){
+                  level = 1;
+                  $state.go('lobby');
+                }
+                else {
+                  $timeout(function(){
+                    stageNumber = 1;
+                    Util.saveLevel($scope.activityId, ++level);
+                    Ctrl17.getConfiguration(level);
+                  }, 1000);
+                }
+              }
+            }
+          },
+          function(err){ $log.error(err); successPlayer.release(); $scope.showText = false; $scope.$apply();}
+        );
+
+        successPlayer.play();
       }
     };
 
@@ -172,7 +194,22 @@
         return _.contains($scope.patternOptions, movedValue);
       }
       else {
-        return $scope.patternLeft.length + $scope.patternRight.length == stageData.patternLength;
+        return $scope.patternLeft.length + $scope.patternRight.length == 2 * stageData.patternLength;
+      }
+    };
+
+    Ctrl17.failure = function(){
+      if (!checking){
+        var failureFeedback = NumberPattern.getFailureAudio();
+
+        failurePlayer = new Media(AssetsPath.getFailureAudio($scope.activityId) + failureFeedback.path,
+          function(){ failurePlayer.release(); $scope.showText = false; $scope.$apply(); checking = false;},
+          function(err){ failurePlayer.release(); $log.error(err); $scope.showText = false; checking = false; $scope.$apply();}
+        );
+
+        $scope.textSpeech = failureFeedback.text;
+        $scope.showText = true;
+        failurePlayer.play();
       }
     };
 
