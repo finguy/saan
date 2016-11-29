c0cc962ae2a4cf26216ae96fe757dbdc40bb8b0e
Leandro Zeballos <leandro.zeballos@bairesdev.com>
2016-11-29 01:18:44 -0300

    Cleaned up controller 12

--- letter-sound-1/www/js/controllers/12-controller.js
+++ letter-sound-1/www/js/controllers/12-controller.js
@@ -3,32 +3,22 @@
   angular.module('saan.controllers')
     .controller('12Ctrl', function($scope, $state, $log, $timeout, RandomText, TTSService,
       Util, Animations, Score, ActividadesFinalizadasService, AssetsPath) {
-      $scope.activityId = 12; // Activity Id
+      $scope.activityId = 12;
       $scope.assetsPath = AssetsPath.getImgs($scope.activityId);
       $scope.img = "";
-      $scope.assets = [];
       $scope.playedTexts = [];
       $scope.text = "";
-      $scope.instructions = ""; // Instructions to read
-      $scope.successMessages = [];
-      $scope.errorMessages = [];
       $scope.showReading = false;
       $scope.showQuestion = false;
       $scope.showOptions = false;
-      $scope.dashboard = []; // Dashboard letters
       $scope.showText = false;
       $scope.textSpeech = "";
       $scope.level = null; // Indicates activity level
-      $scope.totalLevels = 3;
       $scope.activityProgress = 0;
       $scope.score = 0;
       $scope.checkingAnswer = false;
-
-      //Reproduces sound using TTSService
       $scope.speak = TTSService.speak;
-
-
-
+      
       var Ctrl12 = Ctrl12 || {};
       Ctrl12.instructionsPlayer;
       Ctrl12.setUpLevel = function() {
@@ -46,7 +36,6 @@
       };
 
       Ctrl12.successFeedback = function() {
-        //Success feeback player
         var successFeedback = RandomText.getSuccessAudio();
         $scope.textSpeech = successFeedback.text;
         $scope.showText = true;
@@ -66,7 +55,6 @@
       }
 
       Ctrl12.errorFeedback = function() {
-        //Failure feeback player
         var failureFeedback = RandomText.getFailureAudio();
         $scope.textSpeech = failureFeedback.text;
         $scope.showText = true;
@@ -84,19 +72,14 @@
         failurePlayer.play();
       }
 
-      //Shows Activity Dashboard
       Ctrl12.showDashboard = function(readInstructions) {
-
         Ctrl12.setUpLevel();
         Ctrl12.setUpScore();
         Ctrl12.setUpStatus();
-
         RandomText.text($scope.level, $scope.playedTexts).then(
           function success(data) {
             Ctrl12.setUpContextVariables(data);
             var readWordTimeout = (readInstructions) ? 2000 : 1000;
-
-            //wait for UI to load
             $timeout(function() {
               if (readInstructions) {
                 Ctrl12.instructionsPlayer.play();
@@ -121,12 +104,9 @@
         $scope.question = textJson.questions[position].question;
         $scope.answer = parseInt(textJson.questions[position].answer, 10);
         $scope.options = _.shuffle(textJson.questions[position].options);
-
         $scope.instructions = data.instructions;
         $scope.successMessages = data.successMessages;
         $scope.errorMessages = data.errorMessages;
-
-        $scope.assets = data.assets;
         $scope.addScore = data.scoreSetUp.add;
         $scope.substractScore = data.scoreSetUp.substract;
         $scope.minScore = data.scoreSetUp.minScore;
@@ -150,14 +130,14 @@
             Ctrl12.instructionsPlayer.release();
           }
         );
+        Ctrl12.instructionsPlayer.play();
       };
 
       Ctrl12.success = function() {
         $scope.checkingAnswer = true;
         Ctrl12.successFeedback();
         $timeout(function() {
-          //Advance level
-          Ctrl12.levelUp(); //Advance level
+          Ctrl12.levelUp();
           if (!$scope.finished) {
             $scope.score = Score.update($scope.addScore, $scope.activityId, $scope.finished);
             $scope.finished = $scope.level >= Ctrl12.finalizationLevel;
@@ -198,38 +178,27 @@
       $scope.displayQuestion = function() {
         $scope.showReading = false;
         $scope.showQuestion = true;
-        //Wait for UI to load
         $timeout(function() {
           $scope.speak($scope.question);
-          //Wait spoken instructions
           $timeout(function() {
             $scope.showQuestion = false;
             $scope.showOptions = true;
-            $scope.$apply(); //this triggers a $digest
+            $scope.$apply();
           }, 2000);
         }, 1000);
       };
 
-      //Advance one level
       Ctrl12.levelUp = function() {
         $scope.level++;
-        $scope.letters = [];
-        $scope.dashboard = [];
-        $scope.selectedLetters = [];
       };
 
-      // Goes back one level
       Ctrl12.levelDown = function() {
         $scope.level = (level > 1) ? (level - 1) : 1;
-        $scope.numbers = [];
-        $scope.dashboard = [];
-        $scope.selectedNumbers = [];
       };
 
 
       //*************** ACTIONS **************************/
-      //Show Dashboard
-      $scope.$on('$ionicView.beforeLeave', function() {
+      $scope.$on('$ionicView.beforeEnter', function() {
         Ctrl12.showDashboard(true);
       });
       $scope.$on('$ionicView.beforeLeave', function() {
