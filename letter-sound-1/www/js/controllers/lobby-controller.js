87f6f0ebcc9472f5a42844eeb6caf415764326de
Jose Gazzano <jose.gazzano@fing.edu.uy>
2016-12-20 21:55:34 -0300

    adjusting some styles and modifying config data for activities

--- letter-sound-1/www/js/controllers/lobby-controller.js
+++ letter-sound-1/www/js/controllers/lobby-controller.js
@@ -1,42 +1,42 @@
 angular.module('saan.controllers')
 .controller('LobbyCtrl', function($scope, $rootScope, $window, actividadesConfiguradas, ActividadesFinalizadasService, _){
 
-	var chequearActividadesDisponibles = function(){
-		var actividadesDisponibles = [];
-		var actividadesFinalizadas = ActividadesFinalizadasService.get();
-		_.each(actividadesConfiguradas,function(key, actividadConfigurada){
-			if(actividadesConfiguradas[actividadConfigurada]){
-				if((actividadesConfiguradas[actividadConfigurada].dependencies === undefined) ||
-						actividadesConfiguradas[actividadConfigurada].length === 0){
-							actividadesDisponibles.push(actividadConfigurada);
-				}else{
-					var actividadesNecesarias = actividadesConfiguradas[actividadConfigurada].dependencies;
-					if(_.difference(actividadesNecesarias,actividadesFinalizadas).length === 0){
-						actividadesDisponibles.push(actividadConfigurada);
-					}
+  var chequearActividadesDisponibles = function(){
+    var actividadesDisponibles = [];
+    var actividadesFinalizadas = ActividadesFinalizadasService.get();
+    _.each(actividadesConfiguradas,function(key, actividadConfigurada){
+      if (_.findIndex(actividadesDisponibles, function(activity){ return activity == actividadConfigurada; }) == -1){
+      if(actividadesConfiguradas[actividadConfigurada]){
+        if((actividadesConfiguradas[actividadConfigurada].dependencies === undefined) ||
+            actividadesConfiguradas[actividadConfigurada].length === 0){
+              actividadesDisponibles.push(actividadConfigurada);
+        }else{
+          var actividadesNecesarias = actividadesConfiguradas[actividadConfigurada].dependencies;
+          if(_.difference(actividadesNecesarias,actividadesFinalizadas).length === 0){
+            actividadesDisponibles.push(actividadConfigurada);
+          }
+        }
+      }
+    }
+    });
 
 
-				}
-			}
-		});
+    $scope.actividadesDisponibles = _.sortBy(actividadesDisponibles, "order");
+    console.log("lobby check");
+  };
 
+  $scope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
+    if(toState.name === 'lobby'){
+      chequearActividadesDisponibles();
+    }
+  });
 
-		$scope.actividadesDisponibles = actividadesDisponibles;
-		console.log("lobby check");
-	};
+  $scope.$on('$ionicView.beforeEnter', function() {
+    $rootScope.isInLobby = true;
+  });
 
-	$scope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
-		if(toState.name === 'lobby'){
-			chequearActividadesDisponibles();
-		}
-	});
-
-	$scope.$on('$ionicView.beforeEnter', function() {
-	  $rootScope.isInLobby = true;
-	});
-
-	$scope.$on('$ionicView.leave', function() {
-	  $rootScope.isInLobby = false;
-	});
+  $scope.$on('$ionicView.leave', function() {
+    $rootScope.isInLobby = false;
+  });
 
 });
