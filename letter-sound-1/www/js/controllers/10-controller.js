8b6a8cc12282f16e98030f5a6f4afb88294f2f68
Leandro Zeballos <lzeballos@wearenolte.com>
2016-12-29 18:08:54 -0300

    saving progress

--- letter-sound-1/www/js/controllers/10-controller.js
+++ letter-sound-1/www/js/controllers/10-controller.js
@@ -120,8 +120,7 @@
           if (data.allWords[j]) {
             var ER = new RegExp(data.allWords[j], "i");
             if (!ER.test($scope.rimesStr)) {
-              wordsToPlay.push({
-                "letters": data.allWords[j].split(""),
+              wordsToPlay.push({                
                 "word": data.allWords[j]
               });
             }
@@ -130,8 +129,7 @@
 
         wordsToPlay.length = 3;
         wordsToPlay.push({
-          "word": rime,
-          "letters": rime.split("")
+          "word": rime
         });
         $scope.words = _.shuffle(wordsToPlay);
 
@@ -209,6 +207,39 @@
 
       Ctrl10.levelDown = function() {
         $scope.level = (level > 1) ? (level - 1) : 1;
+      };
+
+      //Drag
+      $scope.sortableSourceOptions = {
+        containment: '.activity10',
+        containerPositioning: 'relative',
+        clone: false,
+        dragEnd: function(eventObj) {
+          if (!$scope.sortableTargetOptions.accept(eventObj.source.itemScope, eventObj.dest.sortableScope)){
+            console.log("wrong!!");
+            $scope.handleProgress(false);
+            $scope.draggedWord = false;
+          }
+          else{
+            console.log("move again");
+          }
+        },
+        itemMoved: function (eventObj) {
+          console.log("success");
+          $scope.handleProgress(true);
+        },
+        accept: function(sourceItemHandleScope, destSortableScope){
+          return false;
+        }
+      };
+
+      //Drop
+      $scope.sortableTargetOptions = {
+        accept: function(sourceItemHandleScope, destSortableScope){
+          console.log("accept");
+          var ER = new RegExp(sourceItemHandleScope.modelValue.word,"i");
+          return ER.test($scope.rimesStr);
+        },
       };
 
       //*************** ACTIONS **************************/
